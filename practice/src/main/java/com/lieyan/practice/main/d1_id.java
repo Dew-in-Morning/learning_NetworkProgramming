6f58969cd0ded59c16fe3aee94306a678633f9df
dewfire <3151716799@qq.com>
2025-08-04 22:02:29 +0800

    学习IP地址

---
+++ practice/src/main/java/com/lieyan/practice/main/d1_id.java
@@ -0,0 +1,24 @@
+package com.lieyan.practice.main;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class d1_id {
+    public static void main(String[] args) throws IOException {
+        System.out.println("---测试开始---");
+        System.out.println("test-1");
+        InetAddress ip1=InetAddress.getLocalHost();
+        System.out.println(ip1.getHostAddress());
+        System.out.println(ip1.getHostName());
+
+        System.out.println("test-2");
+        InetAddress ip2=InetAddress.getByName("www.baidu.com");
+        System.out.println(ip2.getHostAddress());
+        System.out.println(ip2.getHostName());
+
+        System.out.println("test-3");
+        boolean reachable = ip2.isReachable(6000);
+        System.out.println("测试结束");
+    }
+}
