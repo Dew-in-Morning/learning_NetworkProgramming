daccac86dfc8edd697c3c9d134adbb7d634c3d4a
dewfire <3151716799@qq.com>
2025-08-08 21:16:52 +0800

    TCP的客户端，一发

--- practice/src/main/java/com/lieyan/practice/main/d4/Client.java
+++ practice/src/main/java/com/lieyan/practice/main/d4/Client.java
@@ -1,4 +1,19 @@
 package com.lieyan.practice.main.d4;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
 public class Client {
+    public static void main(String[] args) throws Exception {
+        Socket socket = new Socket("127.0.0.1",6666);
+        OutputStream os = socket.getOutputStream();
+        DataOutputStream dos = new DataOutputStream(os);
+
+        dos.writeUTF("你爱我我爱你");
+
+        dos.close();
+        socket.close();
+    }
 }
